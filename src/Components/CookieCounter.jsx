4f6421925da40d52a4109beaec0d42ebe94d889d
DAVINA SINGH <davina_singh@outlook.com>
2024-02-15 15:55:35 +0000

    little bit of css

--- src/Components/CookieCounter.jsx
+++ src/Components/CookieCounter.jsx
@@ -1,9 +1,13 @@
+import "../App.css";
+
 // CookieCounter function defined (and exported) - takes count props
 export default function CookieCounter({ count }) {
   return (
     <div>
       {/* p tag that dispalys the number of cookies from the count prop(passed from the parent) */}
-      <p>Number Of Cookies: {count}</p>
+      <p id="numberOfCookies">
+        Number Of Cookies: <p id="cookieCount">{count}</p>
+      </p>
     </div>
   );
 }
--- src/Components/CookieIncrementButton.jsx
+++ src/Components/CookieIncrementButton.jsx
@@ -1,9 +1,12 @@
+import "../App.css";
 // CookieIncrementButton function defined (and exported) and takes 'increment' prop which is a function (and it has been destructured)
 export default function CookieIncrementButton({ increment }) {
   return (
     <div className="cookie-button">
       {/* the cookie button has an event listener on it such that when the cookie is clicked, it calls the increment function which has been passed as props */}
-      <button onClick={increment}>🍪</button>
+      <button id="cookieButton" onClick={increment}>
+        🍪
+      </button>
     </div>
   );
 }
